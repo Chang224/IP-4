98c76194e4406375165191f8404182b227a77c69
Chang Gatere <chang.gatere1@gmail.com>
2021-11-01 17:09:14 +0300

    adding the append feature to the toppings

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,185 @@ $(document).ready(function() {
     $("#pizza-size").append("Large Pizza KES 950")
     });
 });
+
+$(document).ready(function() {
+    $("#thin").click(function() {
+    $("#pizza-size").append("Hand Tossed Thin Crust KES 50")
+    });
+});
+$(document).ready(function() {
+    $("#regular").click(function() {
+    $("#pizza-size").append("Itaian Crust KES 150")
+    });
+});
+$(document).ready(function() {
+    $("#thick").click(function() {
+    $("#pizza-size").append("Thick Crust KES 200")
+    });
+});
+
+$(document).ready(function() {
+    $("#pineapple-small").click(function() {
+    $("#pizza-size").append("Pineapple Small Pizza KES 40")
+    });
+});
+$(document).ready(function() {
+    $("#pineapple-medium").click(function() {
+    $("#pizza-size").append("Pineapple Medium Pizza KES 70")
+    });
+});
+$(document).ready(function() {
+    $("#pineapple-large").click(function() {
+    $("#pizza-size").append("Pineapple Large Pizza KES 100")
+    });
+});
+
+$(document).ready(function() {
+    $("#bacon-1").click(function() {
+    $("#pizza-size").append("Bacon Small Pizza KES 60")
+    });
+});
+$(document).ready(function() {
+    $("#bacon-2").click(function() {
+    $("#pizza-size").append("Bacon Medium Pizza KES 90")
+    });
+});
+$(document).ready(function() {
+    $("#bacon-3").click(function() {
+    $("#pizza-size").append("Bacon Large Pizza KES 120")
+    });
+});
+
+$(document).ready(function() {
+    $("#olives1").click(function() {
+    $("#pizza-size").append("Olives Small Pizza KES 40")
+    });
+});
+$(document).ready(function() {
+    $("#olives2").click(function() {
+    $("#pizza-size").append("Olives Medium Pizza KES 70")
+    });
+});
+$(document).ready(function() {
+    $("#olives3").click(function() {
+    $("#pizza-size").append("Olives Large Pizza KES 40")
+    });
+});
+
+$(document).ready(function() {
+    $("#mushroom--1").click(function() {
+    $("#pizza-size").append("Mushroom Small Pizza KES 40")
+    });
+});
+$(document).ready(function() {
+    $("#mushroom--2").click(function() {
+    $("#pizza-size").append("Mushroom Medium Pizza KES 70")
+    });
+});
+$(document).ready(function() {
+    $("#mushroom--3").click(function() {
+    $("#pizza-size").append("Mushroom Large Pizza KES 100")
+    });
+});
+
+$(document).ready(function() {
+    $("#onions11").click(function() {
+    $("#pizza-size").append("Onion Small Pizza KES 40")
+    });
+});
+$(document).ready(function() {
+    $("#onions22").click(function() {
+    $("#pizza-size").append("Onions Medium Pizza KES 70")
+    });
+});
+$(document).ready(function() {
+    $("#onions33").click(function() {
+    $("#pizza-size").append("Onions Large Pizza KES 100")
+    });
+});
+
+$(document).ready(function() {
+    $("#chicken-01").click(function() {
+    $("#pizza-size").append("Chicken Small Pizza KES 60")
+    });
+});
+$(document).ready(function() {
+    $("#chicken-02").click(function() {
+    $("#pizza-size").append("Chicken Medium Pizza KES 90")
+    });
+});
+$(document).ready(function() {
+    $("#chicken-03").click(function() {
+    $("#pizza-size").append("Chicken Large Pizza KES 120")
+    });
+});
+
+$(document).ready(function() {
+    $("#extra-cheese_1").click(function() {
+    $("#pizza-size").append("Extra Cheese Small Pizza KES 60")
+    });
+});
+$(document).ready(function() {
+    $("#extra-cheese_2").click(function() {
+    $("#pizza-size").append("Extra Cheese Pizza KES 90")
+    });
+});
+$(document).ready(function() {
+    $("#extra-cheese_3").click(function() {
+    $("#pizza-size").append("Etra Cheese Large Pizza KES 120")
+    });
+});
+
+$(document).ready(function() {
+    $("#pepperoni-p").click(function() {
+    $("#pizza-size").append("Pepperoni Small Pizza KES 60")
+    });
+});
+$(document).ready(function() {
+    $("#pepperoni-pp").click(function() {
+    $("#pizza-size").append("Pepperoni Medium Pizza KES 90")
+    });
+});
+$(document).ready(function() {
+    $("#pepperoni-ppp").click(function() {
+    $("#pizza-size").append("Pepperoni Large Pizza KES 120")
+    });
+});
+
+$(document).ready(function() {
+    $("#avacado1").click(function() {
+    $("#pizza-size").append("Avacado Small Pizza KES 40")
+    });
+});
+$(document).ready(function() {
+    $("#avacado2").click(function() {
+    $("#pizza-size").append("Avacado Medium Pizza KES 70")
+    });
+});
+$(document).ready(function() {
+    $("#avacado3").click(function() {
+    $("#pizza-size").append("Avacado Large Pizza KES 100")
+    });
+});
+
+$(document).ready(function() {
+    $("#sausaged").click(function() {
+    $("#pizza-size").append("Sausage Small Pizza KES 60")
+    });
+});
+$(document).ready(function() {
+    $("#sausagedd").click(function() {
+    $("#pizza-size").append("Sausage Medium Pizza KES 90")
+    });
+});
+$(document).ready(function() {
+    $("#sausageddd").click(function() {
+    $("#pizza-size").append("Sausage Large Pizza KES 120")
+    });
+});
+
+$(document).ready(function() {
+    $(":button").click(function() {
+    $("#pizza-size").append("Delivery fee KES 120")
+    });
+});
