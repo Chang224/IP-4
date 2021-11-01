5648a4f60e773d7bbfe6b19813a975d48130114b
Chang Gatere <chang.gatere1@gmail.com>
2021-11-01 13:50:56 +0300

    adding a button and an alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
     $("#delivery").click(function() {
     $("#delivery-textbox").show();
+    $(":button").show();
     });
 });
 $(document).ready(function() {
     $("#city-centre-branch-pick-up").click(function() {
     $("#delivery-textbox").hide();
+    $(":button").hide();
     });
 });
 $(document).ready(function() {
     $("#westlands-branch-pick-up").click(function() {
     $("#delivery-textbox").hide();
+    $(":button").hide();
     });
 });
 $(document).ready(function() {
     $("#lavington-branch-pick-up").click(function() {
     $("#delivery-textbox").hide();
+    $(":button").hide();
     });
 });
 $(document).ready(function() {
     $("#karen-branch-pick-up").click(function() {
     $("#delivery-textbox").hide();
+    $(":button").hide();
+    });
+});
+$(document).ready(function() {
+    $(":button").click(function() {
+    alert("Thank you!! Your pizza will be delivered in one hour.")
     });
 });
