33a7a94df05a45a4874e480cbbe96615d5cd0151
Chang Gatere <chang.gatere1@gmail.com>
2021-11-01 13:30:56 +0300

    toggle back on the delivery textarea

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
     $("#delivery-textbox").show();
     });
 });
+$(document).ready(function() {
+    $("#city-centre-branch-pick-up").click(function() {
+    $("#delivery-textbox").hide();
+    });
+});
+$(document).ready(function() {
+    $("#westlands-branch-pick-up").click(function() {
+    $("#delivery-textbox").hide();
+    });
+});
+$(document).ready(function() {
+    $("#lavington-branch-pick-up").click(function() {
+    $("#delivery-textbox").hide();
+    });
+});
+$(document).ready(function() {
+    $("#karen-branch-pick-up").click(function() {
+    $("#delivery-textbox").hide();
+    });
+});
