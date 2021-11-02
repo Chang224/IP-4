c9fdfd98640b0fd761b4591fe4fe2ae951909511
Chang Gatere <chang.gatere1@gmail.com>
2021-11-02 15:16:55 +0300

    adding the new button with a new alert

--- js/scripts.js
+++ js/scripts.js
@@ -29,206 +29,211 @@ $(document).ready(function() {
     });
 });
 $(document).ready(function() {
-    $(":button").click(function() {
+    $("#submit").click(function() {
     alert ("Your order will be delivered to your location. Thank you!!")
     });
 });
 
 $(document).ready(function() {
     $("#small").click(function() {
-    $("#pizza-size").append("Small Pizza KES 450")
+    $("#pizza-size").append("Small Pizza <i>KES 450</i>")
     });
 });
 
 $(document).ready(function() {
     $("#medium").click(function() {
-    $("#pizza-size").append("Medium Pizza KES 700")
+    $("#pizza-size").append("Medium Pizza <i>KES 700</i>")
     });
 });
 $(document).ready(function() {
     $("#large").click(function() {
-    $("#pizza-size").append("Large Pizza KES 950")
+    $("#pizza-size").append("Large Pizza <i>KES 950</i>")
     });
 });
-
 $(document).ready(function() {
     $("#thin").click(function() {
-    $("#pizza-size").after("Hand Tossed Thin Crust KES 50")
+    $("#pizza-size").after("Hand Tossed Thin Crust <i>KES 50</i>")
     });
 });
 $(document).ready(function() {
     $("#regular").click(function() {
-    $("#pizza-size").after("Itaian Crust KES 150")
+    $("#pizza-size").after("Itaian Crust <i>KES 150</i>")
     });
 });
 $(document).ready(function() {
     $("#thick").click(function() {
-    $("#pizza-size").after("Thick Crust KES 200")
+    $("#pizza-size").after("Thick Crust <i>KES 200</i>")
     });
 });
 
 $(document).ready(function() {
     $("#pineapple-small").click(function() {
-    $("#pizza-size").after("Pineapple Small Pizza KES 40")
+    $("#pizza-size").after("Pineapple Small Pizza <i>KES 40</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#pineapple-medium").click(function() {
-    $("#pizza-size").after("Pineapple Medium Pizza KES 70")
+    $("#pizza-size").after("Pineapple Medium Pizza <i>KES 70</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#pineapple-large").click(function() {
-    $("#pizza-size").after("Pineapple Large Pizza KES 100")
+    $("#pizza-size").after("Pineapple Large Pizza <i>KES 100</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#bacon-1").click(function() {
-    $("#pizza-size").after("Bacon Small Pizza KES 60")
+    $("#pizza-size").after("Bacon Small Pizza <i>KES 60</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#bacon-2").click(function() {
-    $("#pizza-size").after("Bacon Medium Pizza KES 90")
+    $("#pizza-size").after("Bacon Medium Pizza <i>KES 90</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#bacon-3").click(function() {
-    $("#pizza-size").after("Bacon Large Pizza KES 120")
+    $("#pizza-size").after("Bacon Large Pizza <i>KES 120</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#olives1").click(function() {
-    $("#pizza-size").after("Olives Small Pizza KES 40")
+    $("#pizza-size").after("Olives Small Pizza <i>KES 40</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#olives2").click(function() {
-    $("#pizza-size").after("Olives Medium Pizza KES 70")
+    $("#pizza-size").after("Olives Medium Pizza <i>KES 70</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#olives3").click(function() {
-    $("#pizza-size").after("Olives Large Pizza KES 40")
+    $("#pizza-size").after("Olives Large Pizza <i>KES 40</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#mushroom--1").click(function() {
-    $("#pizza-size").after("Mushroom Small Pizza KES 40")
+    $("#pizza-size").after("Mushroom Small Pizza <i>KES 40</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#mushroom--2").click(function() {
-    $("#pizza-size").after("Mushroom Medium Pizza KES 70")
+    $("#pizza-size").after("Mushroom Medium Pizza <i>KES 70</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#mushroom--3").click(function() {
-    $("#pizza-size").after("Mushroom Large Pizza KES 100")
+    $("#pizza-size").after("Mushroom Large Pizza <i>KES 100</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#onions11").click(function() {
-    $("#pizza-size").after("Onion Small Pizza KES 40")
+    $("#pizza-size").after("Onion Small Pizza <i>KES 40</i>" + "<br>" )
     });
 });
 $(document).ready(function() {
     $("#onions22").click(function() {
-    $("#pizza-size").after("Onions Medium Pizza KES 70")
+    $("#pizza-size").after("Onions Medium Pizza <i>KES 70</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#onions33").click(function() {
-    $("#pizza-size").after("Onions Large Pizza KES 100")
+    $("#pizza-size").after("Onions Large Pizza <i>KES 100</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#chicken-01").click(function() {
-    $("#pizza-size").after("Chicken Small Pizza KES 60")
+    $("#pizza-size").after("Chicken Small Pizza <i>KES 60</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#chicken-02").click(function() {
-    $("#pizza-size").after("Chicken Medium Pizza KES 90")
+    $("#pizza-size").after("Chicken Medium Pizza <i>KES 90</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#chicken-03").click(function() {
-    $("#pizza-size").after("Chicken Large Pizza KES 120")
+    $("#pizza-size").after("Chicken Large Pizza <i>KES 120</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#extra-cheese_1").click(function() {
-    $("#pizza-size").after("Extra Cheese Small Pizza KES 60")
+    $("#pizza-size").after("Extra Cheese Small Pizza <i>KES 60</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#extra-cheese_2").click(function() {
-    $("#pizza-size").after("Extra Cheese Pizza KES 90")
+    $("#pizza-size").after("Extra Cheese Pizza <i>KES 90</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#extra-cheese_3").click(function() {
-    $("#pizza-size").after("Etra Cheese Large Pizza KES 120")
+    $("#pizza-size").after("Etra Cheese Large Pizza <i>KES 120</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#pepperoni-p").click(function() {
-    $("#pizza-size").after("Pepperoni Small Pizza KES 60")
+    $("#pizza-size").after("Pepperoni Small Pizza <i>KES 60</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#pepperoni-pp").click(function() {
-    $("#pizza-size").after("Pepperoni Medium Pizza KES 90")
+    $("#pizza-size").after("Pepperoni Medium Pizza <i>KES 90</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#pepperoni-ppp").click(function() {
-    $("#pizza-size").after("Pepperoni Large Pizza KES 120")
+    $("#pizza-size").after("Pepperoni Large Pizza <i>KES 120</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#avacado1").click(function() {
-    $("#pizza-size").after("Avacado Small Pizza KES 40")
+    $("#pizza-size").after("Avacado Small Pizza <i>KES 40</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#avacado2").click(function() {
-    $("#pizza-size").after("Avacado Medium Pizza KES 70")
+    $("#pizza-size").after("Avacado Medium Pizza <i>KES 70</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#avacado3").click(function() {
-    $("#pizza-size").after("Avacado Large Pizza KES 100")
+    $("#pizza-size").after("Avacado Large Pizza <i>KES 100</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
     $("#sausaged").click(function() {
-    $("#pizza-size").after("Sausage Small Pizza KES 60")
+    $("#pizza-size").after("Sausage Small Pizza <i>KES 60</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#sausagedd").click(function() {
-    $("#pizza-size").after("Sausage Medium Pizza KES 90")
+    $("#pizza-size").after("Sausage Medium Pizza <i>KES 90</i>" + "<br>")
     });
 });
 $(document).ready(function() {
     $("#sausageddd").click(function() {
-    $("#pizza-size").after("Sausage Large Pizza KES 120")
+    $("#pizza-size").after("Sausage Large Pizza <i>KES 120</i>" + "<br>")
     });
 });
 
 $(document).ready(function() {
-    $(":button").click(function() {
-    $("#pizza-size").after("Delivery fee KES 120")
+    $("#submit").click(function() {
+    $("#pizza-size").after("Delivery fee <i>KES 120<i/>" + "<br>")
+    });
+});
+
+$(document).ready(function() {
+    $("#place-order").click(function() {
+    alert ("Your order has been placed. Thank you!")
     });
 });
